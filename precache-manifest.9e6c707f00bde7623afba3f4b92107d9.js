37f3e077c1be2a5bbb96203cb100e4fac07815ee
alperkasli <alperkasli@gmail.com>
2019-11-11 22:43:23 +0400

    Updates

---
+++ precache-manifest.9e6c707f00bde7623afba3f4b92107d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b6f196ee2a9d733168ee4e0396ff3a2",
+    "url": "/recruitmentapp/index.html"
+  },
+  {
+    "revision": "0f3d9e34317495d7aa5c",
+    "url": "/recruitmentapp/static/css/main.7facd359.chunk.css"
+  },
+  {
+    "revision": "8851b118708a9b3c2ef4",
+    "url": "/recruitmentapp/static/js/2.b136b403.chunk.js"
+  },
+  {
+    "revision": "0f3d9e34317495d7aa5c",
+    "url": "/recruitmentapp/static/js/main.5a95811c.chunk.js"
+  },
+  {
+    "revision": "1c8544235aa7c2699d25",
+    "url": "/recruitmentapp/static/js/runtime-main.4c0d57ba.js"
+  },
+  {
+    "revision": "1275c8668b43092070fc73cedbf3740c",
+    "url": "/recruitmentapp/static/media/mephoto.1275c866.png"
+  },
+  {
+    "revision": "906ed4b6421577d30758598c5eff7a62",
+    "url": "/recruitmentapp/static/media/person.906ed4b6.png"
+  }
+]);
